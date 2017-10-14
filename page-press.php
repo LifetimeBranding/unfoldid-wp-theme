1d2f0590dbcad2828ae547f5edc46ff5098e9b0f
Phil Fevry <pf@phlfvry.com>
2017-10-14 15:58:39 -0400

    refactored with loop to be more DRY

--- page-press.php
+++ page-press.php
@@ -1,51 +1,44 @@
-<?php
-get_header();
+<?php 
+  $image_root_path = get_template_directory_uri() . '/images/press/';
+
+  // Title, URL, Image
+  $press_content = array(
+    array("CBS Boston WBZ-TV","#", "wbz-tv.jpg"),
+    array("Northshore Mall", "#", "northshore-mall.jpg"),
+    array("Fashion Show Production", "#", "fashion-show-production.png"),
+    array("Style For Hire Certified", "#", "style-for-hired-certified.png")
+  );
 ?>
 
-  <div class="row hero" id="section__press-hero">
-    <div class="col-12">
-      <div class="text-center">
-        <h1 class="text-white header__hero mb-5 py-5">In <span class="text-small font__halcom d-block py-3">the</span> News.</h1>
-      </div>
+<?php get_header(); ?>
+
+<div class="row hero" id="section__press-hero">
+  <div class="col-12">
+    <div class="text-center">
+      <h1 class="text-white header__hero mb-5 py-5">In <span class="text-small font__halcom d-block py-3">the</span> News.</h1>
     </div>
   </div>
-
-  <div class="container" style="max-width: 800px">
-      <div class="text-center m-5">
-        <h1 class="text-darkgray mb-5">Press releases.</h1>
-        <div class="row">
-          <div class="col-sm-12 mx-auto col-md-6">
-            <a href="#">
-              <img src="<?php echo get_template_directory_uri() ?>/images/press/wbz-tv.jpg" class="img-fluid" width="333" height="250">
-              
-            </a>
-            <p style="font-size: 1.3em" class="text-gold my-1">CBS Boston WBZ-TV</p>
-          </div>
-          <div class="col-sm-12 mx-auto col-md-6">
-          <a href="#">
-          <img src="<?php echo get_template_directory_uri() ?>/images/press/northshore-mall.jpg" class="img-fluid" width="333" height="250">
-          
-        </a>
-        <p style="font-size: 1.3em" class="text-gold my-1">Northshore Mall</p>
-          </div>
-          <div class="col-sm-12 mx-auto col-md-6">
-          <a href="#">
-          <img src="<?php echo get_template_directory_uri() ?>/images/press/fashion-show-production.png" class="img-fluid" width="333" height="250">
-          
-        </a>
-        <p style="font-size: 1.3em" class="text-gold my-1">Fashion Show Production</p>
-          </div>
-          <div class="col-sm-12 mx-auto col-md-6">
-          <a href="#">
-          <img src="<?php echo get_template_directory_uri() ?>/images/press/style-for-hired-certified.png" class="img-fluid" width="333" height="250">
-          
-        </a>
-        <p style="font-size: 1.3em" class="text-gold my-1">Style for Hire Certified</p>
-          </div>
-        </div>
-      </div>
 </div>
 
-    <?php
-    get_footer();
-?>
+<div class="container" style="max-width: 800px">
+  <div class="text-center m-5">
+    <h1 class="text-darkgray mb-5">Our work.</h1>
+
+    <div class="row">
+      <!-- Start Loop -->
+      <?php for ($i = 0; $i < sizeof($press_content); $i++) : ?>
+        <div class="col-sm-12 mx-auto col-md-6">
+          <a href="<?php echo $press_content[$i][1] ?>">
+            <img src="<?php echo $image_root_path . $press_content[$i][2] ?>"
+            class="img-fluid" width="333" height="250">
+          </a>
+          <p style="font-size: 1.3em" class="text-gold my-1"><?php echo $press_content[$i][0] ?></p>
+        </div>
+      <?php endfor; ?>
+      <!-- End Loop -->
+    </div>
+
+  </div>
+</div>
+
+<?php get_footer(); ?>
